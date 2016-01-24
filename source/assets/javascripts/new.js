054ea0073802498a26f500d680254ecedabae050
Niten <niten2@yandex.ru>
2016-01-24 10:29:14 +0700

    add slider

--- source/assets/javascripts/new.js
+++ source/assets/javascripts/new.js
@@ -1,11 +1,33 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
-$('.bxslider').bxSlider({
-  adaptiveWidth: true,
-  minSlides: 1,
-  maxSlides: 1,
-  slideWidth: 670,
-  slideMargin: 10
+// $('.bxslider').bxSlider({
+//   adaptiveWidth: true,
+//   adaptiveHeight: true,
+//   minSlides: 1,
+//   maxSlides: 1,
+//   slideWidth: 670,
+//   responsive: true
+//   // slideMargin: 10
+// });
+
+// });
+
+// Can also be used with $(document).ready()
+// $(document).ready(function () {
+//   $('.flexslider').flexslider({
+//     animation: "slide"
+//   });
+// });
+
+
+$(function(){
+  SyntaxHighlighter.all();
 });
-
+$(window).load(function(){
+  $('.flexslider').flexslider({
+    animation: "slide",
+    start: function(slider){
+      $('body').removeClass('loading');
+    }
+  });
 });
